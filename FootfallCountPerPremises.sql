8d6d7b658115722da942daede0fcf67a1223beea
Michael Siva <michael.siva@blueyonder.co.uk>
2021-07-29 12:55:33 +0100

    Updated Formatting

--- FootfallCountPerPremises.sql
+++ FootfallCountPerPremises.sql
@@ -1,13 +1,13 @@
-SELECT Businesses.Business,
-	   ISNULL(Premises.StreetNo,'') AS 'StreetNo',
-	   Premises.Street,
-	   Premises.PostCode,
-	   ISNULL(SUM(Count),0) AS 'FootfallCount'
+SELECT		Businesses.Business,
+			ISNULL(Premises.StreetNo,'') AS 'StreetNo',
+			Premises.Street,
+			Premises.PostCode,
+			ISNULL(SUM(Count),0) AS 'FootfallCount'
 FROM Footfall WITH (NOLOCK) 
 RIGHT JOIN Premises WITH (NOLOCK) ON Footfall.PremisesId=Premises.Id
 INNER JOIN Businesses WITH (NOLOCK) ON Premises.BusinessId= Businesses.Id
-GROUP BY PremisesId,
-		Businesses.Business,
-		Premises.StreetNo,
-		Premises.Street,
-		Premises.PostCode
+GROUP BY	PremisesId,
+			Businesses.Business,
+			Premises.StreetNo,
+			Premises.Street,
+			Premises.PostCode
